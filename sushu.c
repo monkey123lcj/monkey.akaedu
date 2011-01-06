876e95e4ba61c4acddc5745a6c8c08ee494e3580
monkey <monkey@monkey.(none)>
2011-01-06 11:19:13 +0800

    sushu

---
+++ sushu.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+ void main(void)
+{
+	int i,j;
+	float k;
+	int m=0;
+	for(i=101;i<200;i++)
+	{
+	k=sqrt(i);
+	for(j=2;j<k;j++)
+	{
+		 if(i%j==0)
+		m=1;
+		else
+		m=0;
+	}
+	if(m==1)
+	printf("the num is %5d",i);
+	}
+
+
+}
+
